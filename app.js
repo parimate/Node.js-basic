dd736229b6aefdafbc9b66dd95f1eb34820b1007
First_Parimate <58456371+parimate@users.noreply.github.com>
2022-05-05 15:43:57 +0700

    Add all file of Project
    Hello World

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'))
+app.get("/", (req,res) =>{ 
+    res.send('Hello World');
+})
+
+app.listen(port, ()=>{
+    debug("Listening on port",port);
+})
