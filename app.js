d23b5a7488bf2e9158ca75b013c68bdc9f750695
First_Parimate <58456371+parimate@users.noreply.github.com>
2022-05-07 09:28:01 +0700

    add router

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
-const products = require("./data/products.json")
-const productRouter = express.Router();
+
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+const productsRouter = require("./src/router/productsRouter");
 
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "public/")));
@@ -14,21 +14,7 @@ app.use(express.static(path.join(__dirname, "public/")));
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-productRouter.route("/").get((req, res) => {
-  res.render("products",{
-    products,
-  }
-  );
-});
-
-productRouter.route("/:id").get((req, res) => {
-    const id = req.params.id;
-  res.render("Product ",{
-      product: products[id],
-  })
-});
-
-app.use("/products", productRouter);
+app.use("/products", productsRouter);
 
 app.get("/", (req, res) => {
   res.render("index", {
